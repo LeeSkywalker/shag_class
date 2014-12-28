86dcf7a89854b647d7dbf3202b1ff9b3afe33ae2
alyona- <alyona_om@mail.ru>
2014-12-28 17:23:47 +0300

    2.Пример реализации строкового массива

---
+++ 14task/2.stringArray.cpp
@@ -0,0 +1,12 @@
+/*2.Пример инициализации строкового массива*/
+#include <iostream>
+
+using namespace std;
+
+int n = 5;
+char line[] = "Cat!";//Инициализация строкового массива
+void main(){
+	cout << "Word: ";
+	//Показ на экран строкового массива
+	cout << line;
+}
